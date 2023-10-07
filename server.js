b076fb1023b5bc6af39ba8fc8af1dc64338f7acb
sam <a0983131803@gmail.com>
2023-10-07 12:54:26 +0800

    connect db

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./configs/config');
+const user = require('./routes/api/user')
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'nodedashboarddb'
+    })
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error);
+    });
+
+
+
+app.listen(config.port, () => {
+    console.log(`Listening on port ${config.port}`);
 });
 
 app.get('/test', (req, res) => {
