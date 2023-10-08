2ae24a737d714329bccce79bdaf22f337f393db9
sam <a0983131803@gmail.com>
2023-10-08 08:19:56 +0800

    add DB name

--- configs/config.js
+++ configs/config.js
@@ -1,5 +1,6 @@
 module.exports={
-    mongoURI:"mongodb+srv://admin:admin@cluster0.wi8skmd.mongodb.net/?retryWrites=true&w=majority",
+    mongoURI:"mongodb+srv://admin:admin@cluster0.lkvjyiq.mongodb.net/?retryWrites=true&w=majority",
     port:5000,
+    DB:"nodedashboarddb",
     secret:"secret"
 }
