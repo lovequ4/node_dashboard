559d0a1c17a7d0824dc3034616b1948c07223440
sam <a0983131803@gmail.com>
2023-10-10 07:53:44 +0800

    add Quasar

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
-import './style.css'
+import router from './router'
 import App from './App.vue'
+import { Quasar,Notify } from 'quasar'
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
 
-createApp(App).mount('#app')
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+const app = createApp(App)
+
+app.use(Quasar, {
+    plugins: { Notify }, // import Quasar plugins and add here
+  })    
+app.use(router)
+app.mount('#app')
